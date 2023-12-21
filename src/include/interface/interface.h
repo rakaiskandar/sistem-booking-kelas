59a70c3d73c70827c8094b3eef8c1393a7b551c1
rakaiskandar <iskandarraka4@gmail.com>
2023-12-21 19:48:17 +0700

    style: format interface

--- src/include/interface/interface.h
+++ src/include/interface/interface.h
@@ -3,24 +3,27 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-typedef struct Users{
+typedef struct Users
+{
     char username[12];
     char password[12];
     char role[10];
 } users;
 
-typedef struct Book{
-	char kode_kelas[20];
+typedef struct Book
+{
+    char kode_kelas[20];
     char prodi[20];
     char nama[20];
-	char fakultas[20];
+    char fakultas[20];
     char tanggal[20];
     bool status_pemesanan;
 } books;
 
-typedef struct Class{
-   char kode_kelas[20];
-   char gedung[20];
+typedef struct Class
+{
+    char kode_kelas[20];
+    char gedung[20];
 } classes;
 
 #endif
